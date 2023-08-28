bf94026aec6988e91dded331b2e4758f9fb74675
daeyong <cys964@gmail.com>
2023-08-28 20:30:29 +0900

    nav 컴포넌트 전체 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
+import Layout from '@/components/layout/Layout';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <SessionProvider session={session}>
-            {/* <Layout> */}
-            <Component {...pageProps} />
-            {/* </Layout> */}
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
         </SessionProvider>
     );
 }
