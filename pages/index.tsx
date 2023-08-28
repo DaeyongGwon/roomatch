f1452cde1ea62f27c609720066d171441af8daa9
daeyong <cys964@gmail.com>
2023-08-28 20:30:57 +0900

    소셜 로그인 기본 페이지에서 로그인 페이지로 코드 이동

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,26 +10,5 @@ export default function Signin() {
             </>
         );
     }
-    return (
-        <>
-            <button
-                className="w-full transform rounded-md bg-gray-700 px-4 py-2 tracking-wide text-white transition-colors duration-200 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none"
-                onClick={() => signIn('kakao')}
-            >
-                kakao login
-            </button>
-            <button
-                className="w-full transform rounded-md bg-gray-700 px-4 py-2 tracking-wide text-white transition-colors duration-200 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none"
-                onClick={() => signIn('naver')}
-            >
-                naver login
-            </button>
-            <button
-                className="w-full transform rounded-md bg-gray-700 px-4 py-2 tracking-wide text-white transition-colors duration-200 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none"
-                onClick={() => signIn('google')}
-            >
-                google login
-            </button>
-        </>
-    );
+    return <>메인</>;
 }
