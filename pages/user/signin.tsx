a62555439c906074be5b914851c190f268f0500f
ox01220122 <ox0122@gmail.com>
2023-08-29 18:48:52 +0900

    form 제출

--- pages/user/signin.tsx
+++ pages/user/signin.tsx
@@ -4,8 +4,20 @@ import Image from 'next/image';
 import kakaoButton from '@/public/image/kakao_button.png';
 import GoogleButton from '@/public/image/google_button.png';
 import naverButton from '@/public/image/naver_button.png';
+import Link from 'next/link';
+import { useState } from 'react';
+import axios from 'axios';
 export default function Signin() {
     const { data: session } = useSession();
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const handleLogin = () => {
+        // 로그인 처리 또는 데이터 전송 로직을 구현합니다
+        console.log('사용자 이름:', username);
+        console.log('비밀번호:', password);
+        // 폼 데이터 전송을 원하신다면 아래 코드를 추가합니다
+        // document.getElementById("loginForm")?.submit();
+    };
     if (session) {
         return (
             <>
@@ -19,22 +31,38 @@ export default function Signin() {
             <div className={styles['login-container']}>
                 <h1>Login</h1>
                 <div className={styles['login-form']}>
-                    <form>
+                    <form id="loginForm">
                         <fieldset>
                             <div>
-                                {/* <label className={styles['user-label']}></label> */}
-                                <input type="text" placeholder="ID" className={styles['user-input-label']} />
+                                <input
+                                    type="text"
+                                    placeholder="ID"
+                                    className={styles['user-input-label']}
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
                             </div>
                             <br />
                             <div>
-                                {/* <label className={styles['user-label']}></label> */}
-                                <input type="password" placeholder="PW" className={styles['user-input-label']} />
+                                <input
+                                    type="password"
+                                    placeholder="PW"
+                                    className={styles['user-input-label']}
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
                             </div>
                         </fieldset>
                     </form>
-                </div>
 
-                <div>회원가입</div>
+                    <button onClick={handleLogin} className={styles['signin-button']}>
+                        로그인
+                    </button>
+
+                    <Link href="/user/signup">
+                        <div className={styles['signup-button']}>회원가입</div>
+                    </Link>
+                </div>
 
                 <div className={styles['social-login']}>
                     <Image
