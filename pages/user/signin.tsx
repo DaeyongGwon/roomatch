2eb50f9b5070624a3419b3f909fa16f7708619b0
ox01220122 <ox0122@gmail.com>
2023-08-29 18:07:57 +0900

    signin 페이지 수정

--- pages/user/signin.tsx
+++ pages/user/signin.tsx
@@ -1,5 +1,9 @@
 import { signIn, useSession, signOut } from 'next-auth/react';
 import styles from '@/styles/user.module.css';
+import Image from 'next/image';
+import kakaoButton from '@/public/image/kakao_button.png';
+import GoogleButton from '@/public/image/google_button.png';
+import naverButton from '@/public/image/naver_button.png';
 export default function Signin() {
     const { data: session } = useSession();
     if (session) {
@@ -13,31 +17,44 @@ export default function Signin() {
     return (
         <>
             <div className={styles['login-container']}>
-                <h1>로그인창</h1>
+                <h1>Login</h1>
                 <div className={styles['login-form']}>
                     <form>
-                        <div>
-                            <label className={styles['user-label']}>아이디</label>
-                            <input type="text" />
-                        </div>
-                        <br />
-                        <div>
-                            <label className={styles['user-label']}>패스워드</label>
-                            <input type="password" />
-                        </div>
+                        <fieldset>
+                            <div>
+                                {/* <label className={styles['user-label']}></label> */}
+                                <input type="text" placeholder="ID" className={styles['user-input-label']} />
+                            </div>
+                            <br />
+                            <div>
+                                {/* <label className={styles['user-label']}></label> */}
+                                <input type="password" placeholder="PW" className={styles['user-input-label']} />
+                            </div>
+                        </fieldset>
                     </form>
                 </div>
 
+                <div>회원가입</div>
+
                 <div className={styles['social-login']}>
-                    <button className={styles.social} onClick={() => signIn('kakao')}>
-                        kakao login
-                    </button>
-                    <button className={styles.social} onClick={() => signIn('naver')}>
-                        naver login
-                    </button>
-                    <button className={styles.social} onClick={() => signIn('google')}>
-                        google login
-                    </button>
+                    <Image
+                        src={kakaoButton}
+                        style={{ width: '50px', height: '50px', padding: '6px' }}
+                        alt="Kakao"
+                        onClick={() => signIn('kakao')}
+                    />
+                    <Image
+                        src={GoogleButton}
+                        style={{ width: '50px', height: '50px', padding: '6px' }}
+                        alt="google"
+                        onClick={() => signIn('google')}
+                    />
+                    <Image
+                        src={naverButton}
+                        style={{ width: '50px', height: '50px', padding: '6px' }}
+                        alt="naver"
+                        onClick={() => signIn('naver')}
+                    />
                 </div>
             </div>
         </>
