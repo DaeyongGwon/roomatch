a85b8d4bcd82fbebfa6c84207ba45ed920ccf087
daeyong <cys964@gmail.com>
2023-08-30 12:24:55 +0900

    [API] signup api 작성 완료

--- pages/api/user/signup/index.ts
+++ pages/api/user/signup/index.ts
@@ -1,31 +1,28 @@
-const db = require('@/common/config/db/db');
 import { NextApiRequest, NextApiResponse } from 'next';
-import isLogin from '@/utils/login';
-import { getSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react'; // Import getSession from next-auth/react
+const db = require('@/common/config/db/db');
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
-        const user_id = await isLogin(req, res);
-        if (user_id) return res.redirect('/');
+        const session = await getSession({ req }); // Get session using getSession
+        if (session?.user) return res.redirect('/');
         res.status(200).json({ page: 'signup' });
     } else if (req.method === 'POST') {
-        const { userid, password } = req.body;
-        const createUserQuery = `
-      INSERT INTO user (id, pw)
-      VALUES (${userid},${password})
-    `;
-        try {
-            // const result = db.query(createUserQuery, function (err: any, result: any) {});
-            // const userId = result.insertId;
+        const { user_id, password, name, email } = req.body;
 
+        const createUserQuery = `
+             INSERT INTO user (id, pw, name, email)
+             VALUES ('${user_id}', '${password}','${name}','${email}')
+        `;
+        try {
             db.query(createUserQuery, function (err: any, result: any) {
                 if (err) {
                     console.error('에러 발생:', err);
+                    res.status(500).json({ result: false });
                 } else {
                     const userId = result.insertId;
                     console.log('새로운 사용자 ID:', userId);
-                    // 가상의 세션 설정
-                    req.session.user = userId;
-                    res.status(200).json({ result: true, name: userid, password });
+                    // req.session.user = userId; // Remove this line
+                    res.status(200).json({ result: true, data: user_id, password, name, email });
                 }
             });
         } catch (err) {
