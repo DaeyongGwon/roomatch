18ee864cc1e0ac3941c05338ca7d9fe138f120e1
daeyong <cys964@gmail.com>
2023-09-04 12:24:50 +0900

    signin api

--- pages/api/user/signin/index.ts
+++ pages/api/user/signin/index.ts
@@ -1,50 +1,44 @@
-import jwt from 'jsonwebtoken';
-import config from '@/config/token';
-import isLogin from '@/utils/login';
-import { promisify } from 'util'; // Node.js의 util 모듈 사용
 import { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcrypt';
+import { signJwtAccessToken } from '@/lib/jwt';
+
 const db = require('@/common/config/db/db');
-class AuthenticationError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'AuthenticationError';
-    }
-}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const user_id = await isLogin(req, res);
-    if (user_id) return res.redirect('/');
-    try {
-        const { userid, password } = req.body;
-        // db.query() 콜백을 프로미스로 변환
-        const queryAsync = promisify(db.query).bind(db);
-        const [user] = await queryAsync(
-            `SELECT * FROM user WHERE id = ${db.escape(userid)} AND pw = ${db.escape(password)}`,
-        );
-        console.log('dddd', user);
-        if (!user) throw new Error('유저 정보 없음');
-        const id = user.id;
-        const access = jwt.sign({ id }, config.ACCESS_TOKEN!, {
-            expiresIn: '1h',
-        });
-        const refresh = jwt.sign({ id }, config.REFRESH_TOKEN!, {
-            expiresIn: '7d',
-        });
-        await queryAsync(`UPDATE user SET refresh = '${refresh}' WHERE id = '${id}'`);
-        if (req.body.keep == 'on') {
-            console.log('test1');
-            res.setHeader('Set-Cookie', [
-                `access=${access}; HttpOnly; Secure; Max-Age=${60 * 60 * 24 * 30}; Path=/`,
-                `refresh=${refresh}; HttpOnly; Secure; Path=/`,
-            ]).redirect('/');
-        } else {
-            console.log('test2');
-            res.setHeader('Set-Cookie', [
-                `access=${access}; HttpOnly; Secure; Path=/`,
-                `refresh=${refresh}; HttpOnly; Secure; Path=/`,
-            ]).redirect('/');
+    if (req.method === 'POST') {
+        try {
+            const { user_id, password } = req.body;
+
+            // Configure MySQL
+            const [rows] = await db.promise().query('SELECT * FROM user WHERE id = ?', [user_id]);
+
+            if (rows.length > 0) {
+                const user = rows[0];
+
+                // 비밀번호 비교
+                const passwordMatch = await bcrypt.compare(password, user.password);
+
+                if (passwordMatch) {
+                    const { password, ...userWithoutPass } = user;
+                    const accessToken = signJwtAccessToken(userWithoutPass);
+
+                    const result = {
+                        ...userWithoutPass,
+                        accessToken,
+                        user: userWithoutPass, // user 정보를 추가합니다.
+                    };
+
+                    return res.status(200).json(result);
+                }
+            }
+
+            // 사용자를 찾을 수 없거나 비밀번호가 일치하지 않는 경우
+            return res.status(401).json({ message: '인증 실패' });
+        } catch (error) {
+            console.error('[MySQL 오류]:', error);
+            return res.status(500).json({ message: '서버 오류' });
         }
-    } catch (err) {
-        console.log('콘솔 로깅', err);
-        res.status(401).json({ message: '인증 오류' });
+    } else {
+        return res.status(405).json({ message: '잘못된 요청 메서드' });
     }
 }
